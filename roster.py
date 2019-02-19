d5fa37000bc18aaa618fcaf08edee39ed24382cf
Kyle Miller <kylesmiller38@gmail.com>
2019-02-18 21:07:31 -0700

    added remove player function and ensured the same person cannot join the roster more than once

--- roster.py
+++ roster.py
@@ -5,8 +5,10 @@ class Roster():
         self.__name = str(name)
         self.__slots = int(size)
         self.__admin = str(admin)
-        self.__registeredPlayers = [""] * self.__slots
-        self.__waitList = [""] * (self.__slots // 2)
+        self.__registeredPlayers = ["OPEN SLOT"] * self.__slots
+        self.__registeredPlayerIDs = [""] * self.__slots
+        self.__waitList = ["OPEN SLOT"] * (self.__slots // 2)
+        self.__waitListIDs = [""] * (self.__slots // 2)
 
 
     def getName(self):
@@ -48,54 +50,82 @@ class Roster():
             self.__waitList = self.__waitList[:(slotsToRemove * -1)]
 
 
-    def registerPlayer(self, player):
+    def registerPlayer(self, player, playerID=""):
         for i, j in enumerate(self.__registeredPlayers):
-            if j != "":
+            if j == "OPEN SLOT" and j != str(player):
                 self.__registeredPlayers[i] = str(player)
+                if playerID == "":  # no player ID provided
+                    self.__registeredPlayerIDs[i] = player
+                else:
+                    self.__registeredPlayerIDs[i] = playerID
                 return True
             continue
         return False
 
-
-    def waitlistPlayer(self, player):
+    def waitlistPlayer(self, player, playerID=""):
         for i, j in enumerate(self.__waitList):
-            if j != "":
+            if j == "OPEN SLOT" and j != str(player):
                 self.__waitList[i] = str(player)
+                if playerID == "":  # no player ID provided
+                    self.__waitListIDs[i] = player
+                else:
+                    self.__waitListIDs[i] = playerID
                 return True
             continue
         return False
+
+    def attemptRegistery(self, player, playerID=""):
+        if self.registerPlayer(player, playerID):
+            return "R"
+        elif self.waitlistPlayer(player, playerID):
+            return "W"
+        else:
+            return "X"
+
+
+    def removePlayer(self, player):
+        if player in self.__registeredPlayers or player in self.__registeredPlayerIDs:
+            try:
+                playerIndex = self.__registeredPlayers.index(player)
+            except:
+                playerIndex = self.__registeredPlayerIDs.index(player)
+            self.__registeredPlayers.pop(playerIndex)
+            self.__registeredPlayers.index(playerIndex)
+            self.__registeredPlayers.append("")
+            self.__registeredPlayerIDs.append("")
+            return True
+
+        elif player in self.__waitList or player in self.__waitListIDs:
+            try:
+                playerIndex = self.__waitList.index(player)
+            except:
+                playerIndex = self.__registeredPlayerIDs.index(player)
+            self.__waitList.pop(playerIndex)
+            self.__waitListIDs.pop(playerIndex)
+            self.__waitList.append("")
+            self.__waitListIDs.append("")
+            return True
+
+        return False
+
 
 
     def displayPlayers(self):
         # see all registered players without alerting them
-        outPut = "**__" + str(self.__name) + "__**\nREGISTERED"
+        outPut = "**__" + str(self.__name) + "__**\n**REGISTERED**"
         for i in self.__registeredPlayers:
-            if i == "":
-                outPut += "\nOPEN SLOT"
-            else:
-                playerName = str(i)[1:-5].lower()  # remove @ and ID from username to prevent pinging them
-                outPut += "\n" + str(playerName)
-        outPut += "-------------------------\nWAITING LIST"
+            outPut += "\n" + str(i)
+        outPut += "\n-------------------------\n**WAITING LIST**"
         for i in self.__waitList:
-            if i == "":
-                outPut += "\nOPEN SLOT"
-            else:
-                playerName = str(i)[1:-5].lower()  # remove @ and ID from username to prevent pinging them
-                outPut += "\n" + str(playerName)
+            outPut += "\n" + str(i)
         return outPut
 
     def alertPlayers(self):
         # alert all registered players
         outPut = "**__" + str(self.__name) + "__**\nREGISTERED"
-        for i in self.__registeredPlayers:
-            if i == "":
-                outPut += "\nOPEN SLOT"
-            else:
-                outPut += "\n" + str(i)
+        for i in self.__registeredPlayerIDs:
+            outPut += "\n" + str(i)
         outPut += "-------------------------\nWAITING LIST"
-        for i in self.__waitList:
-            if i == "":
-                outPut += "\nOPEN SLOT"
-            else:
-                outPut += "\n" + str(i)
+        for i in self.__waitListIDs:
+            outPut += "\n" + i
         return outPut
