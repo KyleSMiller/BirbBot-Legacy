079895aa294c26d2e3d9e2c058261482bb5bbc6c
Kyle Miller <kylesmiller38@gmail.com>
2019-05-12 21:56:36 -0600

    converted server info class for selenium and bs4 modules instead of requests and lxml

--- serverInfo.py
+++ serverInfo.py
@@ -1,7 +1,12 @@
-from lxml import html
-import requests
 from voiceLines import NoOneHere
 from passwords import queryLoginUsername, queryLoginPassword
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from bs4 import BeautifulSoup
 
 
 # Gracious Welcome:  66.151.138.224:3170  "http://refactor.jp/chivalry/?serverId=1194830"
@@ -11,19 +16,15 @@ noOneHere = NoOneHere()
 
 class ServerInfo:
 
-    def __init__(self, queryAddress, ip, serverName, homePage="https://panel.forcad.org/menu.aspx"):
+    def __init__(self, queryAddress, serverName, homePage="https://panel.forcad.org/menu.aspx"):
         self.__queryAddress = queryAddress
         self.__serverName = serverName
-        self.__ip = ip
-        self.__page = requests.get(self.__queryAddress)
-        self.__tree = html.fromstring(self.__page.content)
-
         self.__home = homePage
-        self.__homePage = requests.get(self.__home)
+        self.__pageSource = ""
 
         self.__map = ""
         self.__population = ""
-        self.__playerList = []
+        self.__playerList = None
 
         self.__chivAdmins = [
             "Baron voŋ Moorland",
@@ -53,132 +54,142 @@ class ServerInfo:
         ]
 
 
-    """
-    Gather all server data and return it in a formatted string
-    :return  formatted string with all relevant server data
-    """
     def getAll(self):
+        """
+        Gather all server data and return it in a formatted string
+        :return  formatted string with all relevant server data
+        """
         session = self.__login(queryLoginUsername, queryLoginPassword)
+        # wait for the login to process before searching for data to scrape
 
-        dataUrl = "https://panel.forcad.org/query.aspx?id=24"  # temporary for testing
-        response = session.get(dataUrl)
-        # print(response.text)
+        WebDriverWait(session, 3).until(EC.presence_of_element_located((By.ID, "ContentPlaceHolder1_div")))
+        self.__getPageSource(session)
+        # gather all relevant server info and return it after it is formatted
+        serverName = self.__getServerName()
+        map = self.__getMap()
+        playerCount = self.__getPlayerCount()
+        playerList = self.__getPlayerList()
+        serverInfo = self.__formatInfo(serverName, map, playerCount, playerList)
+        return serverInfo
 
-
-    """
-    Check the server for a specific player
-    :param player  The player to check the server for
-    :return        True if player is currently connected to server, False if not currently connected
-    """
     def checkFor(self, player):
+        """
+        Check the server for a specific player
+        :param player  The player to check the server for
+        :return        True if player is currently connected to server, False if not currently connected
+        """
         pass
 
-    """
-    Check the server for an administrator
-    :return  True if an admin is currently connected, False if none are currently connected
-    """
     def checkForAdmin(self):
+        """
+        Check the server for an administrator
+        :return  True if an admin is currently connected, False if none are currently connected
+        """
         pass
 
-    """
-    Open a session with provided login data so the server query data can be accessed
-    :param username  username for panel.forcad.org account
-    :param password  password for panel.forcad.org account
-    :return          the session opened with the provided data
-    """
     def __login(self, username, password):
+        """
+        Open a session with provided login data so the server query data can be accessed
+        :param username  username for panel.forcad.org account
+        :param password  password for panel.forcad.org account
+        :return          the session opened with the provided data
+        """
+        browser = webdriver.Chrome()
         loginUrl = "https://panel.forcad.org/"
-        loginUrlPage = requests.get(loginUrl)
-        loginTree = html.fromstring(loginUrlPage.content)
+        browser.get(loginUrl)
+        # find all login elements
+        usernameBox = browser.find_element_by_id("ContentPlaceHolder1_TextBox1")
+        passwordBox = browser.find_element_by_id("ContentPlaceHolder1_TextBox2")
+        loginButton = browser.find_element_by_id("ContentPlaceHolder1_Button1")
+        # input necessary info into login elements and send them
+        usernameBox.send_keys(username)
+        passwordBox.send_keys(password)
+        loginButton.click()
 
-        formData = {
-            "ctl00$ContentPlaceHolder1$TextBox1": username,
-            "ctl00$ContentPlaceHolder1$TextBox2": password,
-            "__EVENTVALIDATION": loginTree.xpath("//*[@id=\"__EVENTVALIDATION\"]/@value"),
-            "__VIEWSTATE": loginTree.xpath("//*[@id=\"__VIEWSTATE\"]/@value"),
-            "__VIEWSTATEGENERATOR": loginTree.xpath("//*[@id=\"__VIEWSTATEGENERATOR\"]/@value")
-        }
+        return browser
 
-        session = requests.Session()
-        r = session.get(loginUrl)  # get cookies necessary for login
-        r = session.post(loginUrl, data=formData, verify=True)
-        print(r.text)
-        rTree = html.fromstring(r.content)
+    def __getPageSource(self, page):
+        pageSource = BeautifulSoup(page.page_source, "lxml")
+        print(pageSource)
 
-        print("Username is " + queryLoginUsername)
-        print("Password is " + queryLoginPassword)
-        print("Posted username is " + str(rTree.xpath('//*[@id="ContentPlaceHolder1_TextBox1"]/@value')))
-        print("Posted password is " + str(rTree.xpath('//*[@id="ContentPlaceHolder1_TextBox2"]/@value')))
-
-        return session
-
-
-    """
-    Refresh the server query info
-    """
-    def __refreshInfo(self):
-        self.__page = requests.get(self.__queryAddress)
-        self.__tree = html.fromstring(self.__page.content)
-
-
-    """
-    Check if the server is online
-    :return  True if online, False if offline
-    """
     def __isOnline(self):
-        tree = html.fromstring(self.__homePage.content)
-        serverStatus = tree.xpath('//*[@id="ContentPlaceHolder1_divStatus"]/text()')
+        """
+        Check if the server is online
+        :return  True if online, False if offline
+        """
+        serverStatus = "Server is running!"
         if serverStatus == "Server is running!":
             return True
         else:
             return False
 
-    """
-    Get the name of the server
-    :return  String of server name
-    """
     def __getServerName(self):
-        return self.__tree.xpath('//*[@id="ContentPlaceHolder1_srvName"]/h4')
+        """
+        Get the name of the server
+        :return  String of server name
+        """
+        return
 
-    """
-    Get the current map the server is running
-    :return  String of the current map
-    """
     def __getMap(self):
-        return self.__tree.xpath('//*[@id="ContentPlaceHolder1_stuff"]/text()[1]')
+        """
+        Get the current map the server is running
+        :return  String of the current map
+        """
+        return
 
-    """
-    Get the 'activePlayers/MaxPlayers' formatted string of the server population
-    :return  String of activePlayers/maxPlayers
-    """
     def __getPlayerCount(self):
-        return self.__tree.xpath('//*[@id="ContentPlaceHolder1_stuff"]/h4[3]')
+        """
+        Get the 'activePlayers/MaxPlayers' formatted string of the server population
+        :return  String of activePlayers/maxPlayers
+        """
+        return
 
-    """
-    Get the number of players currently on the server
-    :return  int of current players
-    """
     def __getCurrentPlayers(self):
+        """
+        Get the number of players currently on the server
+        :return  int of current players
+        """
         playerCountRaw = self.__getPlayerCount()
         currentPlayers = playerCountRaw.split("/")[0]
         return int(currentPlayers)
 
-    """
-    Get the maximum number of players the server supports
-    :return  int of maximum players allowed on the server
-    """
     def __getMaxPlayers(self):
+        """
+        Get the maximum number of players the server supports
+        :return  int of maximum players allowed on the server
+        """
         playerCountRaw = self.__getPlayerCount()
         maxPlayers = playerCountRaw.split("/")[1]
         return int(maxPlayers)
 
-    """
-    Get a list of all players connected to the server
-    :return  list of strings of player names
-    """
     def __getPlayerList(self):
+        """
+        Get a list of all players connected to the server
+        :return  list of strings of player names
+        """
         pass
 
 
-test = ServerInfo("https://panel.forcad.org/query.aspx?id=24", "66.151.138.224:3170", "Gracious Welcome")
+    def __formatInfo(self, serverName, map, playerCount, playerList):
+        """
+        Format all the retrieved server info into a response for BirbBot
+        :param serverName  the name of the server
+        :param map         the map the server is playing
+        :playerCount       the (#/#) formatted string of players on the server
+        :playerList        list of all players currently on the server
+        :return String     the formatted server info response that BirbBot will present
+        """
+        formattedInfo = ("**__" + str(serverName) + "__ is playing __"
+                         + str(map) + "__ with a population of __"
+                         + str(playerCount) + "__**\n")
+        if self.__getCurrentPlayers() != 0:
+            for player in playerList:
+                formattedInfo += "|  " + player + "  |"
+        else:
+            formattedInfo += NoOneHere().getResponse()
+
+        return formattedInfo
+
+
+test = ServerInfo("https://panel.forcad.org/query.aspx?id=24", "Gracious Welcome")
 print(test.getAll())
