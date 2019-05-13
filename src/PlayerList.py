98b385ae632ee0391bf42f655a9cc9e902356e0e
Kyle Miller <kylesmiller38@gmail.com>
2019-05-13 17:02:33 -0600

    added simple format option for displayed player list

--- src/PlayerList.py
+++ src/PlayerList.py
@@ -1,5 +1,6 @@
 from tkinter import font
 import tkinter as tk
+import math
 
 
 class PlayerList:
@@ -9,35 +10,43 @@ class PlayerList:
         self.__playerList = "__**PLAYER LIST:**__\n"
         self.__font = "Whitney"
         self.__fontSize = 10
-        self.__formatPlayerList()
+        self.__largestNameSize = self.__findLargestName()
+        self.__whiteSpaceSize = self.__getPrintedSize(" ")  # size of hairSpace character. member var for performance
+        self.__simpleFormatPlayerList()
 
     def __str__(self):
         return self.__playerList
+
+    def __simpleFormatPlayerList(self):
+        for playerNum, player in enumerate(self.__players):
+            if playerNum % 4 == 0 and playerNum != 0:
+                self.__playerList += "\n"
+            self.__playerList += player + "  |  "
 
     def __formatPlayerList(self):
         """
         Format the players into a table that BirbBot will attach to his server info summary
         """
-        for playerCount, player in enumerate(self.__players):
+        for playerNum, player in enumerate(self.__players):
             cellMargin = self.__findMarginSize(player)
-            self.__playerList += "|"
-            self.__playerList += " " * cellMargin
+            print(int(cellMargin))
+            print(int(cellMargin) + self.__getPrintedSize(player) + int(cellMargin))
+            self.__playerList += " " * int(cellMargin)
             self.__playerList += player
-            self.__playerList += " " * cellMargin
+            self.__playerList += " " * int(cellMargin)
             self.__playerList += "|"
-            if playerCount % 4 == 0:
-                self.__playerList += "\n"
+            if playerNum % 4 == 0:
+                self.__playerList += "\n|"
 
     def __findMarginSize(self, name):
         """
         Find the number of pixels each name must have on either side of it to match the size of the largest cell
         :return: int  The size of each cell in pixels
         """
-        whiteSpaceSize = self.__getPrintedSize(" ", (self.__font, self.__fontSize))  # size of hairSpace character
-        largestCellSize = self.__findLargestName() + (whiteSpaceSize * 4 * 4)  # 2 (4px)spaces on either side of name
+        largestCellSize = self.__largestNameSize + (self.__whiteSpaceSize * 4 * 4)  # 2(4px)spaces on both sides of name
         if largestCellSize % 2 != 0:  # cell size must be even
             largestCellSize += 1
-        marginSize = largestCellSize - self.__getPrintedSize(name, (self.__font, self.__fontSize)) / 2
+        marginSize = math.floor((largestCellSize - self.__getPrintedSize(name))) / 2
 
         return marginSize
 
@@ -48,12 +57,12 @@ class PlayerList:
         """
         largestName = 0
         for player in self.__players:
-            nameSize = self.__getPrintedSize(player, (self.__font, self.__fontSize))
+            nameSize = self.__getPrintedSize(player)
             if nameSize > largestName:
                 largestName = nameSize
         return largestName
 
-    def __getPrintedSize(self, text, printedFont):
+    def __getPrintedSize(self, text, printedFont=("Whitney", 10)):
         """
         Find the width in pixels of a string
         :param text:         The string to find the width of
@@ -64,7 +73,3 @@ class PlayerList:
         printedFont = font.Font(family=printedFont[0], size=printedFont[1])
         (w, h) = (printedFont.measure(text), printedFont.metrics("linespace"))
         return w
-
-testList = PlayerList(["Johnathan"])
-
-testList.test()
